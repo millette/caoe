82aad6525dacaf150a302e0bcaeda52987a1ab08
Robin Millette <robin@millette.info>
2016-09-07 14:10:08 -0400

    Explore, click tags, etc.

--- main.js
+++ main.js
@@ -1,34 +1,63 @@
+/* global $ */
 $(function () {
+  const debug = false
+  const $modal = $('#modal')
   const apiURL = 'https://openclipart.org/search/json/'
   const $sort = $('#sort')
   const $search = $('#search')
+  const $images = $('#images')
+  const $go = $('#go')
+  const $form = $('form')
+
   const imgClick = function (ev) {
     const $self = $(this)
-    ev.preventDefault()
-    $self.fadeOut(function () { $self.remove() })
+    const it = $self.data('info')
+    const directives = {
+      title: { href: function () { return this.detail_link } },
+      tags_array: { label: { text: function () { return this.value } } },
+      svg: { src: function () { return this.svg.url } }
+    }
+    $modal.render(it, directives)
+    $modal.foundation('open')
   }
 
   const addImg = function (img) {
     const $img = $('<img class="thumbnail" src="' + img.svg.png_thumb + '">')
+    $img.data('info', img)
     $img.one('load', function (ev) { $(this).fadeIn() })
     $img.click(imgClick)
     const $el = $('<div class="column small-6 medium-4 large-2">')
     $el.html($img)
-    $('#images').prepend($el)
+    $images.prepend($el)
   }
 
-  const addImgs = function (data) { data.payload.forEach(addImg) }
+  const addImgs = function (data) {
+    $modal.foundation('close')
+    if (debug) { $('pre').text(JSON.stringify(data, null, ' ')) }
+    $go.text('Go').removeClass('warning')
+    $form.prop('disabled', false)
+    $images.empty()
+    data.payload.forEach(addImg)
+  }
 
   const formSubmit = function (ev) {
-    ev.preventDefault()
     const query = {
       query: $search.val(),
       amount: 12,
       sort: $sort.val()
     }
+    ev.preventDefault()
+    $go.text('1s.').addClass('warning')
+    $form.prop('disabled', true)
     $.getJSON(apiURL, query, addImgs)
   }
 
   $(document).foundation()
-  $('form').submit(formSubmit)
+
+  $('.tags_array').on('click', '.label', function (ev) {
+    $search.val($(this).text())
+    formSubmit(ev)
+  })
+
+  $form.submit(formSubmit)
 })
